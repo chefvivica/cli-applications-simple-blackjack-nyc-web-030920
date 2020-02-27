9d9ea84c6e0bc7a95c66140a0b2f2591a6a292af
Vivica Ma <vivicacongma@gmail.com>
2020-02-27 18:23:41 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -55,7 +55,7 @@ invalid_command
 def runner
 welcome
 initial_round = card_total
-  unless card_total > 21
+  until card_total > 21
     card_total = hit?(card_total)
     display_card_total(card_total)
   end 
